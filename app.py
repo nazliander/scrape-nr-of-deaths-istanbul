0785c66fb5df8786b026975ef5493593877b4005
nazliander <nazliander1@gmail.com>
2020-04-07 20:23:56 +0200

    Fix error loop

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def app() -> None:
     process_finished = False
     dates_deaths_dict = {}
     chrome_options = set_chrome_options()
-    batch = deepcopy(missing_dates[100:150])
+    batch = deepcopy(missing_dates)
     while process_finished is False:
         for d in batch:
             time.sleep(1)
@@ -25,12 +25,12 @@ def app() -> None:
                 chrome_options=chrome_options,
                 url=URL)
         missed_vals_in_batch = [
-            vals for vals in dates_deaths_dict.values() if vals is None]
+            (idx, vals) for idx, vals in dates_deaths_dict.items() if vals is None]
         if len(missed_vals_in_batch) == 0:
             process_finished = True
             LOGGER.info("Breaking the loop...")
         else:
-            batch = deepcopy(missed_vals_in_batch)
+            batch = deepcopy([vals[0] for vals in missed_vals_in_batch])
             LOGGER.info("Restarting the loop...")
     for d, record in dates_deaths_dict.items():
         LOGGER.info(f"{get_death_from_record(record)}")
