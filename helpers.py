0ac4260b99e1bad1774aad539f47ff5457b6f50b
nazliander <nazliander1@gmail.com>
2020-04-12 15:17:53 +0200

    Add dockerfile

--- helpers.py
+++ helpers.py
@@ -35,6 +35,8 @@ def set_chrome_options() -> None:
     """
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_prefs = {}
     chrome_options.experimental_options["prefs"] = chrome_prefs
     chrome_prefs["profile.default_content_settings"] = {"images": 2}
